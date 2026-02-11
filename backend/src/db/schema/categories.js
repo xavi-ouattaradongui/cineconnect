313a44aa44b64ddad81d51c9bb26ab9e081a7951
Ouattara Dongui <ouattaradongui123@gmail.com>
2026-02-11 05:27:47 +0100

    connexion backend/frontend (chat de groupre)

--- backend/src/db/schema/categories.js
+++
@@ -1,6 +0,0 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
-
-export const categories = pgTable("categories", {
-  id: serial("id").primaryKey(),
-  name: varchar("name", { length: 50 }).notNull().unique(),
-});
